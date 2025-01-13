724e40e39750c5d737c0b6301841ccade99aaa38
G'lek Tarssza <glek@glektarssza.com>
2025-01-13 20:36:35 +0000

    Added a class to encapsulate the config items.

---
+++ src/main/java/com/glektarssza/highres_screenshots/config/ConfigItems.java
@@ -0,0 +1,23 @@
+package com.glektarssza.highres_screenshots.config;
+
+import com.glektarssza.highres_screenshots.ScreenshotType;
+
+/**
+ * A class which contains the data values for the various configuration fields.
+ */
+public class ConfigItems {
+    /**
+     * The width of the screenshot to take.
+     */
+    public int screenshotWidth;
+
+    /**
+     * The height of the screenshot to take.
+     */
+    public int screenshotHeight;
+
+    /**
+     * The type of file to save the screenshot as.
+     */
+    public ScreenshotType screenshotType;
+}
