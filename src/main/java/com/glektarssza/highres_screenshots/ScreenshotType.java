f6830769e647cf5831285ce14b754d8499475e3c
G'lek Tarssza <glek@glektarssza.com>
2025-01-13 20:52:32 +0000

    Added a screenshot type enum.

---
+++ src/main/java/com/glektarssza/highres_screenshots/ScreenshotType.java
@@ -0,0 +1,21 @@
+package com.glektarssza.highres_screenshots;
+
+/**
+ * The type of file to save the screenshot as.
+ */
+public enum ScreenshotType {
+    /**
+     * Save the screenshot as a PNG.
+     */
+    PNG,
+
+    /**
+     * Save the screenshot as a JPEG.
+     */
+    JPEG,
+
+    /**
+     * Save the screenshot as a BMP.
+     */
+    BMP
+}
